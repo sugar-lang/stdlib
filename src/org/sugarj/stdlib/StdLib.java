5d9a2b00cfd7d8ebbd60d4aebc13c497fcfde23d
Sebastian Erdweg <seba.github@gmail.com>
2014-02-20 15:51:09 +0100

    fix path resolution for windows

--- src/org/sugarj/stdlib/StdLib.java
+++ src/org/sugarj/stdlib/StdLib.java
@@ -1,8 +1,10 @@
 package org.sugarj.stdlib;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
@@ -34,7 +36,12 @@ public class StdLib {
         e.printStackTrace();
       }
     
-    String classPath = thisClassURL.getPath();
+    String classPath;
+    try {
+      classPath = new File(thisClassURL.toURI()).getAbsolutePath();
+    } catch (URISyntaxException e) {
+      classPath = new File(thisClassURL.getPath()).getAbsolutePath();
+    }
     String binPath = classPath.substring(0, classPath.length() - thisClassPath.length());
     
     try {
